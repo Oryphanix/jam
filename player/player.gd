d532475c6d88f03a1410424b6b889ad594cec721
Captos <alphax1273@gamil.com>
2024-10-17 23:07:24 +0100

    restructuring
    started work on enemy

--- player/player.gd
+++ player/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 
-const SPEED = 310.0
+const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
 
@@ -16,7 +16,7 @@ func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("move_left", "move_right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
