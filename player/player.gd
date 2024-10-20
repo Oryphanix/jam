f1adc4d0560b044234ef90d87777792ffa2e04a4
Captos <alphax1273@gamil.com>
2024-10-20 14:25:34 +0100

    merge maanavs changes with mine

--- player/player.gd
+++ player/player.gd
@@ -1,34 +1,44 @@
 extends CharacterBody2D
 
 @export var SPEED = 200.0
+@export var argument = false
+
 const JUMP_VELOCITY = -400.0
+
 #Variable for globalisation of player position
 var playerPosition: Vector2
 var sanity: float
 
-func _physics_process(delta: float) -> void:	
-	#Sets the global player position to the position
-	Global.playerPosition = self.global_position
-	# Add the gravity.
+func _physics_process(delta: float) -> void:
+	if Input.is_action_just_pressed("ui_down") or argument:
+		SPEED = 1100
+		var direction := Input.get_axis("move_left", "move_right")
+		if direction:
+			velocity.x = direction * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, 300)
+		print (SPEED)
+		move_and_slide()
+		argument = true
+		if Input.is_action_just_released("ui_down"):
+			argument = false
+	else:
+		SPEED = 200
+		var direction := Input.get_axis("move_left", "move_right")
+		if direction:
+			velocity.x = direction * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, 200)
+		# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 	else:
 		velocity.y = 0
-
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practie, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("move_left", "move_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
 	move_and_slide()
-	playerPosition = self.global_position
+	Global.playerPosition = self.global_position
 
 
 func _process(delta: float) -> void:
