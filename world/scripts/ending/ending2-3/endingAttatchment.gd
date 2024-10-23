0e8f32008c4abd057d0527857925d4c04d5c9efd
Captos <alphax1273@gamil.com>
2024-10-23 19:37:41 +0100

    temp2

--- world/scripts/ending/ending2-3/endingAttatchment.gd
+++ world/scripts/ending/ending2-3/endingAttatchment.gd
@@ -3,8 +3,13 @@ extends Node
 var delayTimer: Timer
 
 func _ready():
+	wait()
+	#RESET
 	GlobalSceneData.enemyStateName = ""
 	GlobalSceneData.playerSanity = null
+	GlobalSceneData.W0playerPosition = null
+	GlobalSceneData.W0enemyPosition = null
+	GlobalSceneData.W0stairPos = null
 	#World1
 	GlobalSceneData.W1playerPosition = null
 	GlobalSceneData.W1enemyPosition = null
@@ -17,9 +22,14 @@ func _ready():
 	GlobalSceneData.W3playerPosition = null
 	GlobalSceneData.W3enemyPosition = null
 	GlobalSceneData.W3stairPos = null
-
-	pass
-	
+	Global.enemyPosition = Vector2.ZERO
+	Global.enemySpeed =  0
+	Global.playerPosition = Vector2.ZERO
+	Global.playerFace = Vector2.ZERO
+	Global.playerSpeed =  0
+	Global.sanity = 0
+	Global.maxSanity = 0
+	Global.isPlayerhiding = false
 	
 	
 	
@@ -32,5 +42,5 @@ func wait():
 	
 
 func on_timer_finished():
-	get_tree().change_scene_to_file("res://thirdEscape/floor1")
+	get_tree().change_scene_to_file("res://thirdEscape/floor1.tscn")
 	
