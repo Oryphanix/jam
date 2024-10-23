872145538dd34a12184eaf9b9a8e2ca8b41045c2
KrivYaejerit <maanavnagda@gmail.com>
2024-10-23 19:40:02 +0100

    items fixef

--- items/itemscripts/inventory rope.gd
+++ items/itemscripts/inventory rope.gd
@@ -14,4 +14,14 @@ func _on_rope_rope_inventory() -> void:
 	modulate.a8 = 255
 	#revealing it when it is picked up
 	argument = false
+<<<<<<< Updated upstream
 	#stopping it from hiding again
+=======
+#stopping it from hiding again
+
+
+
+func _on_open_window_escape_1() -> void:
+	modulate.a8 = 0
+	argument = true
+>>>>>>> Stashed changes
