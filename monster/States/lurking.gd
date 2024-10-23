2ed3affba17afd1aefbc46d840603cf9be6dba1a
Captos <alphax1273@gamil.com>
2024-10-23 21:45:22 +0100

    aaaaaaaaa

--- monster/States/lurking.gd
+++ monster/States/lurking.gd
@@ -4,7 +4,8 @@ class_name EnemyLurking
 @export var en: CharacterBody2D
 @export var sanityLoss := 500
 var player: CharacterBody2D
-
+var p 
+var sop 
 
 func randCirlePos() -> Vector2:
 	var theta : float = randf() * 2 * PI
@@ -17,22 +18,30 @@ func Enter():
 	print(GlobalSceneData.enemyState)
 	player = get_tree().get_first_node_in_group("Player")
 	var randomPos: Vector2
-	randomPos = (randCirlePos()*Global.playerFOV)+Global.playerPosition
-	while !(randomPos.y < 0 and randomPos.y > -153):
+	randomPos = (randCirlePos()*167.5)+Global.playerPosition
+	while !(randomPos.y < 0 and randomPos.y > -92):
 		randomPos = (randCirlePos()*Global.playerFOV)+Global.playerPosition
 	en.global_position = randomPos
+	p = get_parent()
+	sop = p.get_node("../AnimatedSprite2D")
+	sop.play("lurk")
+	sop.frame = 1
+	sop.stop()
 	en.show()
-	print("IF IS CLIPPING UP, REPLACE 16 WITH 16-MONSTER HEIGHT")
 	
 	
 func Update(_delta: float):
 	var direction = Global.playerPosition - Global.enemyPosition
 	var distance := direction.length()
+
 	if Global.sanity <= (2*Global.maxSanity)/3 and Global.sanity > (Global.maxSanity)/3:
 		SignalManager.transitioned.emit(self, "stalking")
 	if distance > 167.5:
+
 		SignalManager.transitioned.emit(self, "prelurking")
-	elif distance < 15:
+	elif distance < 44:
+		sop.frame = 0 
+		sop.play("lurk")
 		Global.sanity -= sanityLoss
 		SignalManager.transitioned.emit(self, "prelurking")
 	
